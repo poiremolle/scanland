da8fd59fbe9558d6c188550554fa9d48a08b42dc
Melissa Woo <lapoiremolle@gmail.com>
2026-07-15 13:56:15 +0200

    adding randomness to creature initialization parameters

--- Creature.py
+++ Creature.py
@@ -1,16 +1,17 @@
 import pygame
 import random
-from CONSTANTS import SCREEN_WIDTH
+from CONSTANTS import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Creature(pygame.sprite.Sprite):
     def __init__(self, image, *groups):
         super().__init__(*groups)
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
-        # self.speed = random.choice(range(2,5))
-        self.speed = 1
+        self.rect.x = -self.rect.w
+        self.rect.y = random.choice(
+            range(0, SCREEN_HEIGHT - int(SCREEN_HEIGHT * 0.2))
+            )
+        self.speed = random.choice(range(1,5))
 
     def update(self):
         if(self.rect.x > SCREEN_WIDTH):
