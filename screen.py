6642188de4ba7362d5062859c32eeae3b08e8e3b
Melissa Woo <lapoiremolle@gmail.com>
2026-07-15 13:56:14 +0200

    Converting the initialize land function into the LandWindow class

--- screen.py
+++ screen.py
@@ -1,25 +1,28 @@
 import pygame
 from imageprocessing import get_surface_image
 
-def initialize_land():
-    pygame.init()
+class LandWindow:
+    def __init__(self, width=600, height=500):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode([500, 500])
 
-    screen = pygame.display.set_mode([500, 500])
+    def initialize_land(self):
+        running = True
+        while running:
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-    running = True
-    while running:
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            self.screen.fill((230, 215, 255))
 
-        screen.fill((230, 215, 255))
+            surfaceImage = get_surface_image('apple.jpg')
 
-        surfaceImage = get_surface_image('apple.jpg')
+            self.screen.blit(surfaceImage, (0, 0))
 
-        screen.blit(surfaceImage, (0, 0))
+            pygame.display.flip()
 
-        pygame.display.flip()
-
-    pygame.quit()
+        pygame.quit()
 
