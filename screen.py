1912da026660d1c53b96bced525ff051deaf2055
Melissa Woo <lapoiremolle@gmail.com>
2026-07-15 13:56:15 +0200

    adjust cooldown value

--- screen.py
+++ screen.py
@@ -10,7 +10,7 @@ class LandWindow:
         self.height = height
         self.screen = pygame.display.set_mode([SCREEN_WIDTH, 500])
         self.all_sprites = pygame.sprite.Group()
-        self.cooldown = 25
+        self.cooldown = 45
 
     def initialize_land(self):
         running = True
