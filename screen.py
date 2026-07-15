9857674d92e0fc95239726341aaf244a26e8372b
Melissa Woo <lapoiremolle@gmail.com>
2026-07-15 13:56:15 +0200

    adding sprites and groups to the LandWindow constructor and updating them in the main game loop

--- screen.py
+++ screen.py
@@ -7,10 +7,10 @@ class LandWindow:
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode([500, 500])
+        self.all_sprites = pygame.sprite.Group()
 
     def initialize_land(self):
         running = True
-        self.screen.fill((230, 215, 255))
         pygame.display.flip()
 
         while running:
@@ -18,11 +18,18 @@ class LandWindow:
                 if event.type == pygame.QUIT:
                     running = False
 
+            self.all_sprites.update()
+            self.screen.fill((230, 215, 255))
+            self.all_sprites.draw(self.screen)
+            pygame.display.flip()
+
+
         pygame.quit()
 
     def show_img_on_screen(self, imgPath):
         img = remove_white_background(imgPath)
         surface = get_surface(img)
-        self.screen.blit(surface, (0, 0))
+        # Create sprite here 
+        # self.screen.blit(surface, (0, 0))
         pygame.display.flip()
 
