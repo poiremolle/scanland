56d3a5199492642523d0a41d2bfb5659df1e1cf0
Melissa Woo <lapoiremolle@gmail.com>
2026-07-15 13:56:15 +0200

    moving the screen filling outside the display loop and correcting arguments being passed in show_img_on_screen

--- screen.py
+++ screen.py
@@ -10,20 +10,21 @@ class LandWindow:
 
     def initialize_land(self):
         running = True
+        self.screen.fill((230, 215, 255))
+        pygame.display.flip()
+
         while running:
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            self.screen.fill((230, 215, 255))
-
-            pygame.display.flip()
-
         pygame.quit()
 
     def show_img_on_screen(self, imgPath):
+        print(f"Path to image to be shown: {imgPath}")
         img = remove_white_background(imgPath)
-        surface = get_surface_image(img)
+        surface = get_surface(img)
         self.screen.blit(surface, (0, 0))
+        pygame.display.flip()
 
