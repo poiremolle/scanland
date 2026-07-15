78b43be94b49067863af44f73673685945a75797
Melissa Woo <lapoiremolle@gmail.com>
2026-07-15 13:56:15 +0200

    adding a bounce to the Creature

--- Creature.py
+++ Creature.py
@@ -12,11 +12,20 @@ class Creature(pygame.sprite.Sprite):
             range(0, SCREEN_HEIGHT - int(SCREEN_HEIGHT * 0.2))
             )
         self.speed = random.choice(range(1,5))
+        self.bounce_height = [1, 3, 6, 12, -12, -6, -3, -1]
+        self.frame = 0
 
     def update(self):
         if(self.rect.x > SCREEN_WIDTH):
             self.rect.x = -self.rect.w
             return
+        
         self.rect.x += self.speed
+
+        if(self.frame >= len(self.bounce_height)):
+            self.frame = 0
+
+        self.rect.y += self.bounce_height[self.frame]
+        self.frame += 1
 
         
