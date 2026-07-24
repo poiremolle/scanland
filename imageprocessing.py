208137c6565bdd2984113ab538181dc3a786b7eb
Melissa Woo <lapoiremolle@gmail.com>
2026-07-24 20:48:46 +0200

    specify type of argument 'input_queue'

--- imageprocessing.py
+++ imageprocessing.py
@@ -7,7 +7,7 @@ import pygame
 from Creature import Creature 
 
 class ImageProcessor:
-    def __init__(self, input_queue, output_queue: Queue):
+    def __init__(self, input_queue: Queue, output_queue: Queue):
         self.input_queue = input_queue
         self.output_queue = output_queue
         self.working_thread = Thread(target=self.run)
