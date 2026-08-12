eb9d3e9a39852d561fec254232ec172b387f34ad
Melissa Woo <lapoiremolle@gmail.com>
2026-08-12 16:45:34 +0200

    adding a helper function to determine if valid file extension and returning from on_create if false

--- file_watcher.py
+++ file_watcher.py
@@ -1,17 +1,24 @@
 from watchdog.events import FileSystemEventHandler
 from queue import Queue
 from pathlib import Path
+from constants import IMAGE_SUFFIXES
 
 class MyEventHandler(FileSystemEventHandler):
     def __init__(self, image_paths: Queue):
         self.image_paths = image_paths
 
     def on_created(self, event):
-        if event.src_path.endswith(".jpg"):
-            self.image_paths.put(event.src_path)
+        if not self.is_image_suffix(event.src_path):
+            print(f"The file '{event.src_path}' does not have a valid file extension.")
+            return
+        
+        self.image_paths.put(event.src_path) 
 
     def on_deleted(self, event):
          print("Deleted.")
      
     def on_moved(self, event):
          print("Moved.")
+
+    def is_image_suffix(path):
+        return Path(path).suffix.lower() in IMAGE_SUFFIXES
