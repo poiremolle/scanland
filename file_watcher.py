3a6066cae99fd757d7f59ccd0cb68744ea820c8e
Melissa Woo <lapoiremolle@gmail.com>
2026-08-12 16:32:44 +0200

    fix indentation

--- file_watcher.py
+++ file_watcher.py
@@ -1,16 +1,17 @@
 from watchdog.events import FileSystemEventHandler
 from queue import Queue
+from pathlib import Path
 
 class MyEventHandler(FileSystemEventHandler):
-     def __init__(self, image_paths: Queue):
+    def __init__(self, image_paths: Queue):
         self.image_paths = image_paths
 
-     def on_created(self, event):
+    def on_created(self, event):
         if event.src_path.endswith(".jpg"):
             self.image_paths.put(event.src_path)
 
-     def on_deleted(self, event):
+    def on_deleted(self, event):
          print("Deleted.")
      
-     def on_moved(self, event):
+    def on_moved(self, event):
          print("Moved.")
