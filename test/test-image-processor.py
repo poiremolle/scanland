1d1a3fc0b47ad1ebe44eb772a1711ecec3a58dcd
Melissa Woo <lapoiremolle@gmail.com>
2026-08-14 16:38:53 +0200

    adding the processor instance as a fixture

--- test/test-image-processor.py
+++ test/test-image-processor.py
@@ -1,2 +1,10 @@
 import pytest
+from queue import Queue
+from scanland.image_processor import ImageProcessor
 
+@pytest.fixture
+def processor():
+    test_input_queue = Queue()
+    test_output_queue = Queue()
+
+    return ImageProcessor(test_input_queue, test_output_queue)
