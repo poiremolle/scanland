fa7518ed64a988500e83ffcc25bcc2c00c350fdc
Melissa Woo <lapoiremolle@gmail.com>
2026-08-17 11:18:02 +0200

    adding test cases to is_image_suffix test

--- tests/test_file_watcher.py
+++ tests/test_file_watcher.py
@@ -8,5 +8,11 @@ def file_watcher():
     input_queue = Queue()
     return MyEventHandler(input_queue)
 
-def test_is_image_suffix(file_watcher):
+def test_is_image_suffix_true(file_watcher):
     assert file_watcher.is_image_suffix("test.png") == True
+    assert file_watcher.is_image_suffix("test.PNG") == True
+    assert file_watcher.is_image_suffix("test.jpg") == True
+    assert file_watcher.is_image_suffix("test.JPG") == True
+    assert file_watcher.is_image_suffix("test.jpeg") == True
+    assert file_watcher.is_image_suffix("test.JPEG") == True
+
